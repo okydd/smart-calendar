3ade0a4193b531ffbf24bab1a5674748aa43af7f
okydd <okxihu@gmail.com>
2026-08-30 21:14:06 +0800

    deploy 1.9.61 (prev 1.9.60)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 智能日历 Service Worker：离线可用（激进更新版） */
-const CACHE = 'smart-calendar-v3-2026-08-29T13-37-52-205Z';
+const CACHE = 'smart-calendar-v3-2026-08-30T13-09-11-461Z';
 const CORE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
