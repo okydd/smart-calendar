87b4d7c45ebeaedca60cdb0e6d5b194febbcb2e3
okydd <okxihu@gmail.com>
2026-09-08 17:39:00 +0800

    deploy 1.9.72 (prev 1.9.71)

---
+++ versions/1.9.72/assets/index-ExXfbU8T.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CLpzYNsX.js","./index-C-x9ywhj.js","./vendor-react-DFxU1kFX.js","./vendor-antd-DINmExxW.js","./vendor-C8IKlzsv.js","./vendor-antd-BdOndhxL.css","./index-C0heu7r0.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as t}from"./index-C-x9ywhj.js";import"./vendor-react-DFxU1kFX.js";import"./vendor-antd-DINmExxW.js";import"./vendor-C8IKlzsv.js";const a=e("Share",{web:()=>t(()=>import("./web-CLpzYNsX.js"),__vite__mapDeps([0,1,2,3,4,5,6]),import.meta.url).then(r=>new r.ShareWeb)});export{a as Share};
--- versions/1.9.72/sw.js
+++ versions/1.9.72/sw.js
@@ -1,5 +1,5 @@
 /* 智能日历 Service Worker：离线可用（激进更新版） */
-const CACHE = 'smart-calendar-v3-2026-09-07T14-20-02-704Z';
+const CACHE = 'smart-calendar-v3-2026-09-08T09-38-31-704Z';
 const CORE = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
